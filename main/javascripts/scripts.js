6f2b44a9e0fe8b3806bee0054f25348909719dd4
Anna Komkova <ann.komkova98@gmail.com>
2026-02-26 12:40:48 +0300

    js animation

--- main/javascripts/scripts.js
+++ main/javascripts/scripts.js
@@ -1,8 +1,108 @@
-flipCard()
+// flipCard()
 moveProgressBar()
 drawSVG()
 showRandomImage()
 showRandomText()
+animationPopup()
+runningButton()
+changeBlick()
+cursor()
+
+function cursor() {
+  let cursor = document.querySelector('.cursor')
+
+  document.addEventListener('mousemove', (event) => {
+    cursor.style.top = `${event.pageY}px`
+    cursor.style.left = `${event.pageX}px`
+  })
+}
+
+function changeBlick() {
+  let coordX = document.querySelector('.coordX')
+  let coordY = document.querySelector('.coordY')
+  let sectionWH = document.querySelector('.sectionWH')
+  let section = document.querySelector('.changeBlick')
+
+  let img0 = document.querySelector('.blickImg0')
+  let img1 = document.querySelector('.blickImg1')
+  let img2 = document.querySelector('.blickImg2')
+  let img3 = document.querySelector('.blickImg3')
+
+  section.addEventListener('mousemove', (event) => {
+    let cursorX = event.pageX
+    let cursorY = event.pageY
+
+    coordX.innerText = `cursor X: ${cursorX}`
+    coordY.innerText = `cursor Y: ${cursorY}`
+
+    let sectionW = section.getBoundingClientRect().width
+    let sectionH = section.getBoundingClientRect().height
+
+    sectionWH.innerText = `section width: ${sectionW}px; section height: ${sectionH}px`
+
+    let halfSectionW = sectionW / 2
+    let halfSectionH = sectionH / 2
+
+    if (cursorX < halfSectionW && cursorY < halfSectionH) {
+      img0.style.opacity = '1'
+    } else {
+      img0.style.opacity = '0'
+    }
+    if (cursorX > halfSectionW && cursorY < halfSectionH) {
+      img1.style.opacity = '1'
+    } else {
+      img1.style.opacity = '0'
+    }
+    if (cursorX > halfSectionW && cursorY > halfSectionH) {
+      img2.style.opacity = '1'
+    } else {
+      img2.style.opacity = '0'
+    }
+    if (cursorX < halfSectionW && cursorY > halfSectionH) {
+      img3.style.opacity = '1'
+    } else {
+      img3.style.opacity = '0'
+    }
+  })
+}
+
+function runningButton() {
+  let button = document.querySelector('.showAnimEl')
+  let container = document.querySelector('.popupContainer')
+  let section = document.querySelector('.header')
+
+  let sectionWidth = section.getBoundingClientRect().width
+  let sectionHeight = section.getBoundingClientRect().height
+
+  let containerWidth = container.getBoundingClientRect().width
+  let containerHeight = container.getBoundingClientRect().height
+
+  let widthRange = sectionWidth - containerWidth
+  let heightRange = sectionHeight - containerHeight
+
+  button.addEventListener('click', () => {
+    container.style.position = 'absolute'
+    container.style.top = `${Math.random() * heightRange}px`
+    container.style.left = `${Math.random() * widthRange}px`
+  })
+}
+
+function animationPopup() {
+  let popup = document.querySelector('.popup')
+  let button = document.querySelector('.showAnimEl')
+
+  button.addEventListener('click', () => {
+    let popupEl = document.createElement('div')
+    popupEl.classList.add('popupEl')
+    popupEl.innerText = '🤍'
+
+    popup.appendChild(popupEl)
+
+    setTimeout(() => {
+      popupEl.remove()
+    }, 1550)
+  })
+}
 
 function showRandomText() {
   let quotes = [
